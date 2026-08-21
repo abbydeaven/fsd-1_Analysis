ef85edaff8244f7bb7aff0c95d669f7338e35e44
abbydeaven <101464438+abbydeaven@users.noreply.github.com>
2026-08-21 18:15:25 +0000

    add analysis setup script

---
+++ copyfastq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## NOTE!!! Run this from the xfer node ##
+## Create directory to host fastq files and analysis directory ##
+
+## save outdir -- modify if needed
+
+outdir = "./fsd1_Analysis"
+
+mkdir outdir
+mkdir $outdir/fastqFiles
+
+## Copy RNAseq files
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/FSD1_RNAseq $outdir/fastqFiles
+
+## Copy ChIPseq files
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2024_Run143_FASTQ/143-3_ChIP_fsd1-gfpXsad1_GFPtrap* $outdir/fastqFiles/ChIPseq
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2024_Run143_FASTQ/143-5_ChIP_fsd1-gfpXsad1_GFPtrap* $outdir/fastqFiles/ChIPseq
+
+## Copy OX2 genomic DNA for A-to-I editing calls
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2026_Run156_FASTQ/156-N27_Genomic_fsd1GFP2_OX* $outdir/fastqFiles/ChIPseq
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2026_Run156_FASTQ/156-N28_Genomic_fsd1GFP2_OX* $outdir/fastqFiles/ChIPseq
+cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2026_Run156_FASTQ/156-N29_Genomic_fsd1GFP2_OX* $outdir/fastqFiles/ChIPseq
