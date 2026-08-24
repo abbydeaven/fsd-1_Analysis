ded62c5d0d66bd0fdf7df466627ea99c01117775
abbydeaven <101464438+abbydeaven@users.noreply.github.com>
2026-08-24 14:56:54 +0000

    fix chipseq path

--- copyfastq.sh
+++ copyfastq.sh
@@ -13,12 +13,14 @@ mkdir $outdir/fastqFiles
 ## Copy RNAseq files
     echo "Copying RNAseq files ...."
 cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/FSD1_RNAseq $outdir/fastqFiles
+mv $outdir/fastqFiles/FSD1_RNAseq/* $outdir/fastqFiles/FSD1_RNAseq
 
 ## Copy WT RNAseq files from Liu et al. 2017
 cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/FSD1_RNAseq/SRR_Liu2017 $outdir/fastqFiles
 
 ## Copy ChIPseq files
     echo "Copying ChIPseq files ...."
+    mkdir $outdir/fastqFiles/ChIPseq
 cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2024_Run143_FASTQ/143-3_ChIP_fsd1-gfpXsad1_GFPtrap* $outdir/fastqFiles/ChIPseq
 cp -r /project/zallab/SequencingArchive/BaseSpaceDownloads/2024_Run143_FASTQ/143-5_ChIP_fsd1-gfpXsad1_GFPtrap* $outdir/fastqFiles/ChIPseq
     echo "ChIPseq files copied!"
